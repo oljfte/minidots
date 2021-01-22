27ccf06d6dd400a72b07fa2987b37f0632904103
oljfte <oljfte@gmail.com>
2021-01-23 04:42:50 +0900

    Update brew setup script

--- brew/setup.sh
+++ brew/setup.sh
@@ -2,8 +2,7 @@
 
 if ! command -v brew &> /dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    test -d $HOME/.linuxbrew && eval $($HOME/.linuxbrew/bin/brew shellenv)
-    test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 fi
 
 brew bundle --file "$DOTFILES/brew/Brewfile"
