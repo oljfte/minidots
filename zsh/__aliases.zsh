84e7bedc3541f99b30faca836f68fe21655c0c0f
oljfte <oljfte@gmail.com>
2021-01-29 00:24:12 +0900

    Add alias ll

--- zsh/__aliases.zsh
+++ zsh/__aliases.zsh
@@ -3,6 +3,7 @@ source $DOTFILES/zsh/__variables.zsh
 
 alias sudo="sudo "
 alias mv="mv -i"
+alias ll="ls -alG"
 alias psg="ps aux | grep"
 alias relogin="exec $SHELL -l"
 alias dotfiles="cd $DOTFILES"
